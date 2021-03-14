4adc29747cfdbbf3ebc9ef74d9829cae5e5b154c
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-14 17:27:42 -0600

    Nav item created

---
+++ src/Components/NavItem/NavtItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavItem = () => {
+  return (
+    <div className="nav-item-container">
+      <h3 className="nav-item-text"></h3>
+    </div>
+  )
+}
+
+export default NavItem;
