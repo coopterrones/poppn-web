150f1e80db0f6d53571d772b6e55a94f2c17576d
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-14 22:47:03 -0600

    Main layout edits

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Nav from "./Components/Nav/index";
 import React from "react";
 import { Route } from "react-router-dom";
 import Announcement from "./Components/Announcement";
-
+import MainImage from "./Components/MainImage/index";
 function App() {
   return (
     <section className="App">
@@ -11,8 +11,9 @@ function App() {
         <Nav />
       </section>
       <section className="main-container">
-        <Route path="/">
+        <Route exact path="/">
           <Announcement />
+          <MainImage />
         </Route>
       </section>
     </section>
