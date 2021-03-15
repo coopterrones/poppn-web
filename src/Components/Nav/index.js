28f252a04f5f261366b40c7445810b4add8a6423
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-14 19:35:39 -0600

    New divs to accomodate for the logo link creation

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,38 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Nav.scss";
+import Logo from "../Logo/index";
 
 const Nav = () => {
   return (
     <section className="nav-container">
-      <NavLink
-        exact
-        to="/"
-        className="nav-item-link"
-        activeClassName="nav-item-link-active">
-        Home
-      </NavLink>
-      <NavLink
-        exact
-        to="/features"
-        className="nav-item-link"
-        activeClassName="nav-item-link-active">
-        Features
-      </NavLink>
-      <NavLink
-        exact
-        to="/about-us"
-        className="nav-item-link"
-        activeClassName="nav-item-link-active">
-        About Us
-      </NavLink>
-      <NavLink
-        exact
-        to="/contact"
-        className="nav-item-link"
-        activeClassName="nav-item-link-active">
-        Contact
-      </NavLink>
+      <div className="logo-nav-container">
+        <Logo />
+      </div>
+      <div className="nav-links-container">
+        <NavLink
+          exact
+          to="/"
+          className="nav-item-link"
+          activeClassName="nav-item-link-active">
+          Home
+        </NavLink>
+        <NavLink
+          exact
+          to="/features"
+          className="nav-item-link"
+          activeClassName="nav-item-link-active">
+          Features
+        </NavLink>
+        <NavLink
+          exact
+          to="/about-us"
+          className="nav-item-link"
+          activeClassName="nav-item-link-active">
+          About Us
+        </NavLink>
+        <NavLink
+          exact
+          to="/contact"
+          className="nav-item-link"
+          activeClassName="nav-item-link-active">
+          Contact
+        </NavLink>
+      </div>
     </section>
   );
 };
