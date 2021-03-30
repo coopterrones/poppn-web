824ab181599c9b3cbaa00d6c770fca42db720763
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-29 20:29:00 -0600

    Descriptive component creative with sample copy

---
+++ src/Components/Descriptive/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Descriptive.scss";
+
+const Descriptive = () => {
+  return (
+    <div className="descriptive-container">
+      <h2 className="descriptive-header">
+        Seamless payments. Pop in. Pop out.
+      </h2>
+      <p className="descriptive-text">
+        Poppn enables users to easily pay for the tab at any of their favorite
+        bars. Order ahead, order as you move through your night, split the tab
+        with your party, done. Take your night to the next level of convenience
+        with the Poppn Payment App. Drink Responsibly.
+      </p>
+    </div>
+  );
+};
+
+export default Descriptive;
