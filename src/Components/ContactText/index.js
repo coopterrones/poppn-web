aa9e91bdb3c5c034b0da5ef69aa5c9cfe10c9392
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-06 13:19:35 -0600

    ContactText created

---
+++ src/Components/ContactText/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ContactText.scss";
+
+const ContactText = () => {
+  return (
+    <section className="contact-text-form-container">
+      <div className="contact-text-wrapper">
+        <h2 className="contact-text-header">Reach out!</h2>
+        <p className="contact-text-description">
+          Poppn is looking to mold the future of night life. Let's build that
+          future together.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ContactText;
