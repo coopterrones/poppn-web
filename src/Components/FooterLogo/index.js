6fc5fc659978d0d31b6e4a7bd6c5dca55d287145
Cooper Terrones <cooper.terrones@outlook.com>
2021-04-20 16:22:28 -0600

    Footer logo component created for standard color state

---
+++ src/Components/FooterLogo/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./FooterLogo.scss";
+
+const FooterLogo = () => {
+  return (
+    <Link to="/" className="footer-logo-link-container active">
+      poppn.
+    </Link>
+  );
+};
+
+export default FooterLogo;
